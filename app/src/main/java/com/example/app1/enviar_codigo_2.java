e309afd2f53794b51e837888f5647721bfb1b342
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 17:52:13 -0500

    Versión 1.5

---
+++ app/src/main/java/com/example/app1/enviar_codigo_2.java
@@ -0,0 +1,14 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enviar_codigo_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_codigo_2);
+    }
+}
