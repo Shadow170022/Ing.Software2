85b09ec4097c74a73e934a495321fb6cd181fb11
Betzaida171151 <65316001+Betzaida171151@users.noreply.github.com>
2020-05-29 14:20:55 -0500

    Update Mapa.java

--- app/src/main/java/com/example/app1/Mapa.java
+++ app/src/main/java/com/example/app1/Mapa.java
@@ -29,6 +29,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+//Código para poder visualizar el mapa 
 public class Mapa extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int PERMISSION_REQUEST_CODE = 1;
@@ -106,17 +107,10 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(Mapa.this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(Mapa.this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
@@ -126,19 +120,16 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
             }
         }
 
-
-        // ------- GOOGLE MAPS (PREPARACION DE INICIO) -------------------------------------------------------------->
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // ------- GOOGLE MAPS (PREPARACION DE INICIO) -------
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // <------- GOOGLE MAPS (PREPARACION DE INICIO)--------------------------------------------------------------
+        // <------- GOOGLE MAPS (PREPARACION DE INICIO)---------------------------->
     }
 
 
-    // ------- GOOGLE MAPS (MAPA CARGADO) -------------------------------------------------------------->
+    // ------- GOOGLE MAPS (MAPA CARGADO) ----------------->
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -188,8 +179,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
         }
     }
 
-    // <------- GOOGLE MAPS (MAPA CARGADO) --------------------------------------------------------------
-
+    // <------- GOOGLE MAPS (MAPA CARGADO) ------------------------>
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
